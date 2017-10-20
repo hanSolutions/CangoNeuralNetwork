459d3863b48726e87405c8484fd7cf991a34232a
samsonxian <samson_xian729@hotmail.com>
2017-10-19 21:59:00 -0400

    data loader functions extraction

--- common/constants.py
+++ common/constants.py
@@ -1,10 +1,14 @@
 APP_NAME = 'CangoNeuralNetwork'
 
-APP_CANGO_NN = "cango_nn"
+APP_CANGO_NN = "cango_nn_binclass"
 APP_CANGO_CNN = "cango_cnn"
 
 DAT_COL_ID = 'id'
 DAT_COL_LABEL = 'label'
 DAT_COL_PBOC_SPOUSE = 'PBOC_Spouse'
 
+DAT_COL_PBOC_ID = "PBOC_ID"
+DAT_COL_APPLY_CD = "applycd"
+
+# random seed for reproducibility
 random_seed = 123
---
+++ datasets/utils.py
@@ -0,0 +1,59 @@
+import logging
+import numpy as np
+import pandas_ml as pdml
+import common.constants as c
+
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
+
+log = logging.getLogger(__name__)
+
+
+def flat_to_one_hot(labels, categorical=True):
+    encoder = LabelEncoder()
+    y = encoder.fit_transform(labels).astype(np.int32)
+    if categorical:
+        y = np_utils.to_categorical(y)
+    return y
+
+
+def shuffle(data):
+    return np.random.shuffle(data)
+
+
+def data_split2(data, split_ratio):
+    data_size = len(data)
+    split_size = int(data_size * split_ratio)
+
+    split_left = data[split_size:]
+    split_right = data[:split_size]
+    return split_left, split_right
+
+
+def reshape(data, reshape_size):
+    d = reshape_size[0]
+    w = reshape_size[1]
+    l = reshape_size[2]
+
+    resize_td = np.resize(data, (data.shape[0], d * w * l))
+    data_reshaped = resize_td.reshape(resize_td.shape[0], d, w, l)
+    return data_reshaped
+
+
+def smote(data, labels, ratio=0.5):
+    # https://www.jair.org/media/953/live-953-2037-jair.pdf
+    mdf = pdml.ModelFrame(data, target=labels)
+    ratio = int(data.shape[0] * ratio)
+    ratio_dict = {1: ratio}
+
+    sampler = mdf.imbalance.over_sampling.SMOTE(ratio_dict, random_state=c.random_seed)
+    sampled = mdf.fit_sample(sampler)
+
+    num_zero = sampled.target.value_counts()[0]
+    num_one = sampled.target.value_counts()[1]
+
+    label_sampled = sampled.target.values
+    sampled.drop('.target', axis=1, inplace=True)
+    data_sampled = sampled.values
+    return data_sampled, label_sampled, num_zero, num_one
+
